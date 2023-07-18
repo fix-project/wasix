9373ef370027ee68fcdd73162eaa15dc9068173e
Keith Winstein <keithw@cs.stanford.edu>
2023-07-18 00:18:47 -0700

    Add dummy implementation of getpwnam_r/getpwuid_r

--- include/pwd.h
+++ include/pwd.h
@@ -29,6 +29,14 @@ struct passwd *getpwnam(const char *name);
 
 struct passwd *getpwuid(uid_t uid);
 
+int getpwnam_r(const char *name, struct passwd *pwd,
+	       char *buf, size_t buflen,
+	       struct passwd **result);
+
+int getpwuid_r(uid_t uid, struct passwd *pwd,
+	       char *buf, size_t buflen,
+	       struct passwd **result);
+
 #ifdef __cplusplus
 }
 #endif
--- src/pwd.c
+++ src/pwd.c
@@ -18,3 +18,21 @@ struct passwd *getpwnam(const char *name) {
 struct passwd *getpwuid(uid_t uid) {
     return &PASSWD_INFO;
 }
+
+int getpwnam_r(const char *name, struct passwd *pwd,
+	       char *buf, size_t buflen,
+	       struct passwd **result)
+{
+  pwd = &PASSWD_INFO;
+  result = &pwd;
+  return 0;
+}
+
+int getpwuid_r(uid_t uid, struct passwd *pwd,
+	       char *buf, size_t buflen,
+	       struct passwd **result)
+{
+  pwd = &PASSWD_INFO;
+  result = &pwd;
+  return 0;
+}
